c8a6a6be210b45a82137d3b96e03ef4b20028862
Artem Starodubtsev <ayostar@gmail.com>
2022-03-26 17:43:52 +0300

    Базы данных домашка 1 основная

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,17 +1,44 @@
 import csv
 
 from django.core.management.base import BaseCommand
+from django.utils.text import slugify
+
 from phones.models import Phone
+
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
         pass
+        # parser.add_argument('csv_file', nargs = '+', type = str)
 
     def handle(self, *args, **options):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
-        for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+        for item in phones:
+            # phone = Phone()
+            # phone.id = int(item.get('id')),
+            # phone.name = item.get('name'),
+            # phone.image = item.get('image'),
+            # phone.price = item.get('price'),
+            # phone.release_date = item.get('release_date'),
+            # phone.lte_exists = item.get('lte_exists'),
+            # phone.slug = phone.slug_name()
+
+            phone = Phone(
+                id = item.get('id'),
+                name = item.get('name'),
+                image = item.get('image'),
+                price = item.get('price'),
+                release_date = item.get('release_date'),
+                lte_exists = item.get('lte_exists'),
+                slug = slugify(item.get('name'))
+            )
+
+
+            try:
+                phone.save()
+                print(f'Телефон {phone} в базу внесён.')
+            except:
+                print(f'Что-то пошло не так. Телефон {phone} не был внесён')
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+
+    id = models.IntegerField(primary_key = True)
+    name = models.CharField(verbose_name = 'Модель', max_length = 100)
+    price = models.FloatField(verbose_name = 'Цена')
+    image = models.URLField(verbose_name = 'Изображение', max_length = 150)
+    release_date = models.DateField(verbose_name = 'Дата')
+    lte_exists = models.BooleanField(verbose_name = 'Модуль LTE')
+    slug = models.SlugField(verbose_name = 'Slugify URL', max_length = 100, unique = True)
+
+    def __str__(self):
+        return self.name
+
+    # def slug_name(self):
+    #     return slugify(self.name)
