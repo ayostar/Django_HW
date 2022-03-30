cbd146772e66bf7822a47904b1b29f7fc192ecd2
Artem Starodubtsev <ayostar@gmail.com>
2022-03-30 15:05:56 +0300

    Домашка базы данных ORM 2.2

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,39 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, ArticleScope
+
+
+class ArticleScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        tag_count = 0
+        all_tags = []
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+                tag_count += 1
+            if form.cleaned_data.get('tag'):
+                all_tags.append(form.cleaned_data.get('tag').id)
+        if len(all_tags) > len(set(all_tags)):
+            raise ValidationError('Тэги не должны повторяться!')
+        if tag_count > 1:
+            raise ValidationError('Основным может быть только один тэг!')
+        elif tag_count == 0:
+            raise ValidationError('Выберите основной тэг!')
+        return super().clean()
+
+
+class ArticleScopeInline(admin.TabularInline):
+    model = ArticleScope
+    formset = ArticleScopeInlineFormset
+    extra = 4
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ArticleScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
