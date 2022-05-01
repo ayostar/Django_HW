235718c9572f8ac0c41558d158145813c881e4f2
Artem Starodubtsev <ayostar@gmail.com>
2022-05-01 14:04:03 +0300

    CRUD HOMEWORK

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,3 +1,6 @@
+import products as products
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
@@ -7,10 +10,20 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
+    pagination_class = LimitOffsetPagination
 
+
+class ProductSearchFilter(SearchFilter):
+    search_param = 'products'
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [ProductSearchFilter]
+    pagination_class = LimitOffsetPagination
+    search_fields = [
+        # products.id, products.title, products.description
+        'products__id', 'products__title', 'products__description'
+    ]
